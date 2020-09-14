295d275e6ab7031d590e178b54d19514c44f8a83
usfbelhadj <usf.belhadj@gmail.com>
2020-09-14 13:34:35 +0100

    To Json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Export to JSON'''
+import requests
+import json
+import sys
+
+if __name__ == "__main__":
+
+    users_num = sys.argv[1]
+    list_task = []
+    di = {}
+    d1 = {}
+    res = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(users_num))
+    username = json.loads(res.text).get('username')
+    res1 = requests.get('https://jsonplaceholder.typicode.com/todos')
+    dic = json.loads(res1.text)
+    for d in dic:
+        comp = d.get('completed')
+        if d.get('userId') == int(users_num):
+            di = {"task": d.get('title'),
+                  "completed": comp, "username": username}
+            list_task.append(di)
+    d1 = {users_num: list_task}
+    f = open("{}.json".format(users_num), "a")
+    f.write(str(d1))
+    f.close()
