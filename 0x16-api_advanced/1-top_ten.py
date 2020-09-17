e7d05e48a0b91ba0528072b4dfd436d9c51cd4c6
usfbelhadj <usf.belhadj@gmail.com>
2020-09-17 14:01:30 +0100

    Test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     req = requests.get(st, headers=headers)
     data = req.json().get('data', {}).get('children', {})
     if req.status_code != 200 or not data:
-        print("None")
+        print(None)
     data = data[0:10]
     for t in data:
         print(t.get('data', {}).get('title'))
