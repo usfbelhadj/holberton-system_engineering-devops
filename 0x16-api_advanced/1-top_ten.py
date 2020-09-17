826284b8538679f6eaa765f0a92896b03022f98a
usfbelhadj <usf.belhadj@gmail.com>
2020-09-17 13:19:48 +0100

    Test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,12 @@ import requests
 def top_ten(subreddit):
     st = 'https://www.reddit.com/r/'+subreddit+'/hot.json'
     headers = {'User-agent': 'X11; Linux x86_64'}
+    out_put = 'None'
     req = requests.get(st, headers=headers)
-    data = req.json().get('data', {}).get('children', None)
+    data = req.json().get('data', None).get('children', None)
     if req.status_code != 200 or not data:
         print('None')
+        return
     data = data[0:10]
     for t in data:
         print(t.get('data', {}).get('title', None))
