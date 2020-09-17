b7450a826d7aa7c15ec5f39d93e2322ab51cc047
usfbelhadj <usf.belhadj@gmail.com>
2020-09-17 13:01:16 +0100

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Top Ten
+'''
+import requests
+
+
+def top_ten(subreddit):
+    st = 'https://www.reddit.com/r/'+subreddit+'/hot.json'
+    headers = {'User-agent': 'X11; Linux x86_64'}
+    req = requests.get(st, headers=headers)
+    data = req.json().get('data', {}).get('children', None)
+    if req.status_code != 200 or not data:
+        print('None')
+    data = data[0:10]
+    for t in data:
+        print(t.get('data', {}).get('title', None))
