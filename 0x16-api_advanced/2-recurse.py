d1998b24dd494aa9fcf77b1bc3f6bd5cc671a4f1
usfbelhadj <usf.belhadj@gmail.com>
2020-09-17 22:47:44 +0100

    Recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,18 +7,19 @@ Recurse it
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after=''):
     '''recurse'''
     st = 'https://www.reddit.com/r/'+subreddit+'/hot.json?after='+after
     headers = {'User-agent': 'X11; Linux x86_64'}
     req = requests.get(st, headers=headers)
     if req.status_code != 200:
-        print(None)
-        return
+        return None
     data = req.json().get('data', {}).get('children', {})
     for i in data:
         hot_list.append(i.get('data').get('title'))
     after = req.json().get('data', {}).get('after')
-    if after:
+    if not after:
+        print(hot_list)
+        return
+    else:
         return recurse(subreddit, hot_list, after)
-    return hot_list
