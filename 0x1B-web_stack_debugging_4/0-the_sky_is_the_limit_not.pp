a778124b924d7040fc904d8be6d55a01066d83c0
usfbelhadj <usf.belhadj@gmail.com>
2020-10-08 21:49:22 +0100

    Test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 exec { 'change ulimit':
   command => "sed -i 's/15/2000/g' /etc/default/nginx",
-  path    =>'/bin',
+  path    => '/bin',
 }
 
-exec { 'nginx restart':
-  command =>'nginx restart',
-  path    =>'/etc/init.d/',
+-> exec { 'nginx restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
 }
