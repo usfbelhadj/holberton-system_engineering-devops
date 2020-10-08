5ade4bdb50e004495ec2975f856435144adce5e8
usfbelhadj <usf.belhadj@gmail.com>
2020-10-08 15:33:38 +0100

    Fix Puppet_lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
-# Puppet script                                                                                                                                                                                 
+# Puppet script
 
-exec { 'change ulimit':                                                                                                                                                                         
-  command => "sed -i 's/15/2000/g' /etc/default/nginx",                                                                                                                                         
-  path => '/bin',                                                                                                                                                                               
-}                                                                                                                                                                                               
-exec { 'nginx restart':                                                                                                                                                                         
-  command => 'nginx restart',                                                                                                                                                                   
-  path => '/etc/init.d/',                                                                                                                                                                       
+exec { 'change ulimit':
+  command => "sed -i 's/15/2000/g' /etc/default/nginx",
+  path    =>'/bin',
+}
+
+exec { 'nginx restart':
+  command =>'nginx restart',
+  path    =>'/etc/init.d/',
 }
