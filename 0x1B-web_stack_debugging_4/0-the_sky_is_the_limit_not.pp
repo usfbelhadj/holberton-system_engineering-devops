d11fcbc9dbac74fa1b5b49bb0ba5aad5c3a197c5
usfbelhadj <usf.belhadj@gmail.com>
2020-10-08 15:26:04 +0100

    web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,9 @@
-# Puppet script
-
-exec { 'change ulimit':
-  command => "sed -i 's/15/2000/g' /etc/default/nginx",
-  path    => '/bin'
-}
-
-exec { 'nginx restart':
-  command => 'service nginx restart',
-  path    => '/etc/init.d/'
+# Puppet script                                                                                                                                                                                 
+exec { 'change ulimit':                                                                                                                                                                         
+  command => "sed -i 's/15/2000/g' /etc/default/nginx",                                                                                                                                         
+  path => '/bin',                                                                                                                                                                               
+}                                                                                                                                                                                               
+exec { 'nginx restart':                                                                                                                                                                         
+  command => 'nginx restart',                                                                                                                                                                   
+  path => '/etc/init.d/',                                                                                                                                                                       
 }
